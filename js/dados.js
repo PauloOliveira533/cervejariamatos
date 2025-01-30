cc9b7c388308916d8eef032efbf5d1a19b570c0f
PauloOliveira533 <paulo.oliveira_533@hotmail.com>
2025-01-30 00:37:42 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,240 @@
+var MENU = {
+    "cervejas": [
+        {
+            "id": "hong-kong-boba-tea-kit-for-6",
+            "img": "./img/cervejas/Corona.webp",
+            "name": "350ml C/12",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 44
+        },
+        {
+            "id": "guys-caliente-margaritas-for-12",
+            "img": "./img/cervejas/Budweiser.webp",
+            "name": "350ml C/12",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 44
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup",
+            "img": "./img/cervejas/Stella.webp",
+            "name": "350ml C/8",
+            "dsc": "Woodford Reserve Mint Julep Syrup",
+            "price": 37
+        },
+        {
+            "id": "new-orleans-hurricane-mix",
+            "img": "./img/cervejas/Spaten.webp",
+            "name": "350ml C/12",
+            "dsc": "New Orleans Hurricane Mix",
+            "price": 50
+        },
+        {
+            "id": "margarita-mix",
+            "img": "./img/cervejas/Original.webp",
+            "name": "350ml C/12",
+            "dsc": "Margarita Mix",
+            "price": 49
+        },
+        {
+            "id": "woodford-reserve-mint-julep-syrup-2-pack",
+            "img": "./img/cervejas/Becks.webp",
+            "name": "350ml C/12",
+            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
+            "price": 00
+        },
+        {
+            "id": "unicorn-parade-milkshake-kit-for-8",
+            "img": "./img/cervejas/Bohemia.webp",
+            "name": "350ml C/12",
+            "dsc": "Unicorn Parade Milkshake Kit for 8",
+            "price": 40
+        },
+        {
+            "id": "chickpea-chiller-kit-for-6",
+            "img": "./img/cervejas/Brahma.webp",
+            "name": "350ml C/12",
+            "dsc": "Chickpea Chiller Kit for 6",
+            "price": 40
+        },
+        {
+            "id": "15194-old-honey-barn-mint-julep-mixer-200ml",
+            "img": "./img/cervejas/Skol.webp",
+            "name": "350ml C/12",
+            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
+            "price": 39
+        },
+        {
+            "id": "kentucky-derby-mint-julep-gift-set",
+            "img": "./img/cervejas/Antartica.webp",
+            "name": "350ml C/12",
+            "dsc": "Kentucky Derby Mint Julep Gift Set",
+            "price": 38
+        },
+        {
+            "id": "002-charleston-bloody-mary-mix-weekender-bold-and-spicy",
+            "img": "./img/cervejas/Kaiser.png",
+            "name": "350ml C/12",
+            "dsc": "Weekender Charleston Bloody Mary Mix Bold & Spicy",
+            "price": 00
+        }
+   
+    ],
+    "refrigerantes": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/refrigerantes/Coca-Cola.png",
+            "name": "1L",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 00
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/refrigerantes/Fanta.png",
+            "name": "1L",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 00
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/refrigerantes/Guarana.png",
+            "name": "1L",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 00
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/refrigerantes/Pepsi.png",
+            "name": "1L",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 00
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+            "img": "./img/refrigerantes/Sukita.png",
+            "name": "1L",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 00
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": "./img/refrigerantes/Garato.png",
+            "name": "1L",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 00
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": "./img/refrigerantes/Sprite.png",
+            "name": "1L",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 00
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+            "img": "./img/refrigerantes/Splash.png",
+            "name": "1L",
+            "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
+            "price": 00
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+            "img": "./img/refrigerantes/Albano.png",
+            "name": "1L",
+            "dsc": "Chicago Deep Dish Pizza - 4 Pack",
+            "price": 00
+        },
+        {
+            "id": "4-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/refrigerantes/Bare.png",
+            "name": "1L",
+            "dsc": "4 Lou Malnati's Deep Dish Pizzas",
+            "price": 00
+        }
+    ],
+    "destilados": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/destilados/Old Parr.png",
+            "name": "1L",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 00
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/destilados/Johnnie Walker.png",
+            "name": "1L",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 00
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/destilados/Orloff.png",
+            "name": "1L",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 00
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/destilados/Smirnoff.png",
+            "name": "1L",
+            "dsc": "Bone-in Rib Steak",
+            "price": 00
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/destilados/Beefeater.png",
+            "name": "1L",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 00
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/destilados/Ballantines.png",
+            "name": "1L",
+            "dsc": "Peter Luger Porterhouse Steaks",
+            "price": 00
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/destilados/Absolut.png",
+            "name": "1L",
+            "dsc": "Ribeye Prime Steak Gift Box",
+            "price": 00
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/destilados/Chivas Regal.png",
+            "name": "1L",
+            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
+            "price": 00
+        },
+        {
+            "id": "california-reserve-ribeye-steak",
+            "img": "./img/destilados/White Horse.png",
+            "name": "1L",
+            "dsc": "California Reserve Ribeye Steak - 12 oz",
+            "price": 00
+        },
+        {
+            "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
+            "img": "./img/destilados/Tanqueray.png",
+            "name": "1L",
+            "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
+            "price": 00
+        },
+        {
+            "id": "california-reserve-filet-mignon-steak",
+            "img": "./img/destilados/Askov.png",
+            "name": "1L",
+            "dsc": "California Reserve Filet Mignon Steak",
+            "price": 00
+        },
+        {
+            "id": "mesquite-smoked-peppered-beef-tenderloin",
+            "img": "./img/destilados/51.png",
+            "name": "1L",
+            "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
+            "price": 00
+        }
+    ]
+}
